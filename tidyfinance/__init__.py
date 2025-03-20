25de3a5b39f3b08a06c40a420fb342da74ca9b10
Christoph Scheuch <19249734+christophscheuch@users.noreply.github.com>
2025-03-20 18:22:10 +0100

    Export additional data_download functios

--- tidyfinance/__init__.py
+++ tidyfinance/__init__.py
@@ -1,5 +1,15 @@
 from .core import assign_portfolio, estimate_betas, estimate_fama_macbeth
-from .data_download import download_data
+from .data_download import (
+    download_data,
+    download_data_constituents,
+    download_data_factors,
+    download_data_factors_ff,
+    download_data_factors_q,
+    download_data_macro_predictors,
+    download_data_osap,
+    download_data_stock_prices,
+    download_data_wrds,
+)
 from .utilities import (
     list_supported_indexes,
     list_tidy_finance_chapters,
@@ -11,6 +21,14 @@ from .utilities import (
 
 __all__ = [
     "download_data",
+    "download_data_factors",
+    "download_data_factors_ff",
+    "download_data_factors_q",
+    "download_data_macro_predictors",
+    "download_data_wrds",
+    "download_data_constituents",
+    "download_data_stock_prices",
+    "download_data_osap",
     "list_supported_indexes",
     "list_tidy_finance_chapters",
     "open_tidy_finance_website",
